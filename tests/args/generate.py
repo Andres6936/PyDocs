b6663b43f2ba3473b7d84d933a784fed3e7cee1a
Joan Andrés <andres6936@live.com>
2021-05-09 13:00:54 -0500

    Pass of compilation arguments

--- tests/args/generate.py
+++ tests/args/generate.py
@@ -1,9 +1,4 @@
-import sys, os
-
-lcldoc = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
-sys.path.insert(0, lcldoc)
-
 from cldoc import cmdgenerate
 
 if __name__ == '__main__':
-    cmdgenerate.run('-I../../example/transport -fPIC --')
+    cmdgenerate.run('-I/home/andres6936/CLionProjects/cldoc-master/example/transport -fPIC --')

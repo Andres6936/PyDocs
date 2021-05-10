54d9d57249058a15dd48173464118f1285453f46
Joan Andrés <andres6936@live.com>
2021-05-09 22:47:57 -0500

    Define the severity level

---
+++ cldoc/logger/level.py
@@ -0,0 +1,13 @@
+from enum import IntEnum, unique
+
+
+@unique
+class Level(IntEnum):
+    EMERGENCY = 0
+    ALERT = 1
+    CRITICAL = 2
+    ERROR = 3
+    WARNING = 4
+    NOTICE = 5
+    INFORMATIONAL = 6
+    DEBUG = 7

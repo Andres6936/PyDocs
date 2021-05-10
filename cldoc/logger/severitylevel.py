b283d31cfc60db7d3050d18788341543c6ddf295
Joan Andrés <andres6936@live.com>
2021-05-09 22:50:04 -0500

    Added documentation

--- cldoc/logger/severitylevel.py
+++ cldoc/logger/severitylevel.py
@@ -3,6 +3,20 @@ from enum import IntEnum, unique
 
 @unique
 class SeverityLevel(IntEnum):
+    """
+    The meaning of severity levels other than Emergency and Debug are relative
+    to the application. For example, if the purpose of the system is to process
+    transactions to update customer account balance information, an error in
+    the final step should be assigned Alert level. However, an error occurring
+    in an attempt to display the ZIP code of the customer may be assigned Error
+    or even Warning level.
+
+    The server process which handles display of messages usually includes all
+    lower (more severe) levels when display of less severe levels is requested.
+    That is, if messages are separated by individual severity, a Warning level
+    entry will also be included when filtering for Notice, Info and Debug
+    messages.
+    """
     EMERGENCY = 0
     ALERT = 1
     CRITICAL = 2

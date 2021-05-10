1338e103e63078ad7d1b3b28e93178eaa9e6bf3a
Joan Andrés <andres6936@live.com>
2021-05-09 23:26:40 -0500

    Use of private method

--- cldoc/logger/consolelogger.py
+++ cldoc/logger/consolelogger.py
@@ -6,30 +6,30 @@ from logger.severitylevel import SeverityLevel
 
 class ConsoleLogger(ILogger):
     def emergency(self, msg):
-        self.message(SeverityLevel.EMERGENCY, msg)
+        self.__message(SeverityLevel.EMERGENCY, msg)
 
     def alert(self, msg):
-        self.message(SeverityLevel.ALERT, msg)
+        self.__message(SeverityLevel.ALERT, msg)
 
     def critical(self, msg):
-        self.message(SeverityLevel.CRITICAL, msg)
+        self.__message(SeverityLevel.CRITICAL, msg)
 
     def error(self, msg):
-        self.message(SeverityLevel.ERROR, msg)
+        self.__message(SeverityLevel.ERROR, msg)
 
     def warning(self, msg):
-        self.message(SeverityLevel.WARNING, msg)
+        self.__message(SeverityLevel.WARNING, msg)
 
     def notice(self, msg):
-        self.message(SeverityLevel.NOTICE, msg)
+        self.__message(SeverityLevel.NOTICE, msg)
 
     def informational(self, msg):
-        self.message(SeverityLevel.INFORMATIONAL, msg)
+        self.__message(SeverityLevel.INFORMATIONAL, msg)
 
     def debug(self, msg):
-        self.message(SeverityLevel.DEBUG, msg)
+        self.__message(SeverityLevel.DEBUG, msg)
 
     @staticmethod
-    def message(severity_level: SeverityLevel, msg: str):
+    def __message(severity_level: SeverityLevel, msg: str):
         sys.stdout.write("{} - {}".format(
             SeverityLevel.abbreviation(severity_level), msg))

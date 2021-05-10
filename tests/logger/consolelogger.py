ddb852ec4cccd9554e2df3a4d12551912f628949
Joan Andrés <andres6936@live.com>
2021-05-09 23:23:54 -0500

    Added test for the console logger

---
+++ tests/logger/consolelogger.py
@@ -0,0 +1,10 @@
+from logger.consolelogger import ConsoleLogger
+
+
+def test():
+    console_logger = ConsoleLogger()
+    console_logger.informational('Informational Message')
+
+
+if __name__ == '__main__':
+    test()

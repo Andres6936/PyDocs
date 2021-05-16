380039f2ad16973811a8eb5d783793ea7d89b88e
Joan Andrés <andres6936@live.com>
2021-05-15 21:32:38 -0500

    Added test glob

--- tests/test_glob.py
+++ tests/test_glob.py
@@ -5,7 +5,9 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_glob_cpp_sources(self):
         self.headers = glob.glob("input/Include/Doryen/**", recursive=True)
+        self.sources = glob.glob("input/Source/**", recursive=True)
         self.assertTrue(len(self.headers) == 89)
+        self.assertTrue(len(self.sources) == 64)
 
 
 if __name__ == '__main__':

1e62ceca3571e3749e2c58cea72b2bdbd40dd4dc
Joan Andrés <andres6936@live.com>
2021-05-16 17:16:31 -0500

    Use of rigth inclusion directory

--- tests/args/generate.py
+++ tests/args/generate.py
@@ -1,4 +1,4 @@
 from cldoc import cmdgenerate
 
 if __name__ == '__main__':
-    cmdgenerate.run('-I/home/andres6936/CLionProjects/cldoc-master/example/transport -fPIC --')
+    cmdgenerate.run('-I/home/andres6936/CLionProjects/cldoc-master/example -fPIC --')

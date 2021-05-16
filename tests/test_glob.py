f34c2353b31fdac525939c107c52351e45b896f5
Joan Andrés <andres6936@live.com>
2021-05-15 22:11:03 -0500

    Use of provider source

--- tests/test_glob.py
+++ tests/test_glob.py
@@ -6,12 +6,12 @@ from files.provider_source import ProviderSource
 
 class MyTestCase(unittest.TestCase):
     def test_glob_cpp_sources(self):
-        self.headers = glob.glob("input/Include/Doryen/**", recursive=True)
-        self.sources = glob.glob("input/Source/**", recursive=True)
-        self.assertTrue(len(self.headers) == 89)
-        self.assertTrue(len(self.sources) == 64)
+        self.headers = ProviderSource("input/Include/Doryen/**")
+        self.sources = ProviderSource("input/Source/**")
+        self.assertTrue(len(self.headers) == 64)
+        self.assertTrue(len(self.sources) == 42)
 
-        self.files = ProviderSource().sources
+        self.files = ProviderSource()
         self.assertTrue(len(self.files) == 0)
 
 

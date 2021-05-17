aa5d5b37e667f71b4dc6da7a59aeeedde2b28275
Joan Andrés <andres6936@live.com>
2021-05-16 19:58:39 -0500

    Added the test for prefix inclusion

--- tests/paths/test_includepaths.py
+++ tests/paths/test_includepaths.py
@@ -1,6 +1,6 @@
 import unittest
 
-from includepaths import _extract_include_paths
+from includepaths import _extract_include_paths, _add_prefix_of_inclusion
 
 
 class MyTestCase(unittest.TestCase):
@@ -18,6 +18,12 @@ class MyTestCase(unittest.TestCase):
         compilation_flags = '-I/home/path/directory -fPIC -xc++'
         compilation_flags = _extract_include_paths(compilation_flags)
         self.assertTrue(compilation_flags == '-I/home/path/directory')
+
+    def test_add_prefix_inclusion(self):
+        paths = ['/home/path/directory', '/dev/path/directory']
+        paths = _add_prefix_of_inclusion(paths)
+        self.assertTrue(tuple(paths) == ('-I/home/path/directory', '-I/dev/path/directory'))
+
 
 
 if __name__ == '__main__':

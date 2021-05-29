3598972817de470b2ae5b14b59b38702b225cfef
Joan Andrés <andres6936@live.com>
2021-05-29 18:04:05 -0500

    Moved class

---
+++ cldoc/clang/objects/compilation_database.py
@@ -0,0 +1,48 @@
+from clang.exceptions.compilation_database import CompilationDatabaseError
+from clang.objects.clang_object import ClangObject
+
+
+class CompilationDatabase(ClangObject):
+    """
+    The CompilationDatabase is a wrapper class around
+    clang::tooling::CompilationDatabase
+
+    It enables querying how a specific source file can be built.
+    """
+
+    def __del__(self):
+        conf.lib.clang_CompilationDatabase_dispose(self)
+
+    @staticmethod
+    def from_result(res, fn, args):
+        if not res:
+            raise CompilationDatabaseError(0,
+                                           "CompilationDatabase loading failed")
+        return CompilationDatabase(res)
+
+    @staticmethod
+    def fromDirectory(buildDir):
+        """Builds a CompilationDatabase from the database found in buildDir"""
+        errorCode = c_uint()
+        try:
+            cdb = conf.lib.clang_CompilationDatabase_fromDirectory(buildDir,
+                                                                   byref(errorCode))
+        except CompilationDatabaseError as e:
+            raise CompilationDatabaseError(int(errorCode.value),
+                                           "CompilationDatabase loading failed")
+        return cdb
+
+    def getCompileCommands(self, filename):
+        """
+        Get an iterable object providing all the CompileCommands available to
+        build filename. Returns None if filename is not found in the database.
+        """
+        return conf.lib.clang_CompilationDatabase_getCompileCommands(self,
+                                                                     filename)
+
+    def getAllCompileCommands(self):
+        """
+        Get an iterable object providing all the CompileCommands available from
+        the database.
+        """
+        return conf.lib.clang_CompilationDatabase_getAllCompileCommands(self)

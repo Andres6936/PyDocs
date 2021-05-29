29f350f1b62e0ae4afd3b825d0f1864fa257cc3e
Joan Andrés <andres6936@live.com>
2021-05-29 14:42:48 -0500

    Moved property

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -79,6 +79,7 @@ import collections
 import sys
 from typing import Tuple
 
+from clang.decorators.cached_property import CachedProperty
 from clang.token_kinds import TokenKinds
 
 if sys.version_info[0] == 3:
@@ -195,30 +196,6 @@ class TranslationUnitSaveError(Exception):
 
 
 ### Structures and Utility Classes ###
-
-class CachedProperty(object):
-    """Decorator that lazy-loads the value of a property.
-
-    The first time the property is accessed, the original property function is
-    executed. The value it returns is set as the new value of that instance's
-    property, replacing the original method.
-    """
-
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
-        try:
-            self.__doc__ = wrapped.__doc__
-        except:
-            pass
-
-    def __get__(self, instance, instance_type=None):
-        if instance is None:
-            return self
-
-        value = self.wrapped(instance)
-        setattr(instance, self.wrapped.__name__, value)
-
-        return value
 
 
 class _CXString(Structure):

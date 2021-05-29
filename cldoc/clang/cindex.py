525a74f0192f97b5049d06f76f7a3ffe101ea464
Joan Andrés <andres6936@live.com>
2021-05-29 15:31:21 -0500

    Moved class

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -82,6 +82,7 @@ from typing import Tuple
 from clang.decorators.cached_property import CachedProperty
 from clang.exceptions.lib_clang import LibclangError
 from clang.exceptions.translation_unit import TranslationUnitLoadError, TranslationUnitSaveError
+from clang.kinds.base_enumeration import BaseEnumeration
 from clang.token_kinds import TokenKinds
 from clang.utility.fix_it import FixIt
 from clang.utility.token_kind import TokenKind
@@ -512,50 +513,6 @@ class TokenGroup(object):
 
 
 ### Cursor Kinds ###
-class BaseEnumeration(object):
-    """
-    Common base class for named enumerations held in sync with Index.h values.
-
-    Subclasses must define their own _kinds and _name_map members, as:
-    _kinds = []
-    _name_map = None
-    These values hold the per-subclass instances and value-to-name mappings,
-    respectively.
-
-    """
-
-    def __init__(self, value):
-        if value >= len(self.__class__._kinds):
-            self.__class__._kinds += [None] * (value - len(self.__class__._kinds) + 1)
-        if self.__class__._kinds[value] is not None:
-            raise ValueError('{0} value {1} already loaded'.format(
-                str(self.__class__), value))
-        self.value = value
-        self.__class__._kinds[value] = self
-        self.__class__._name_map = None
-
-    def from_param(self):
-        return self.value
-
-    @property
-    def name(self):
-        """Get the enumeration name of this cursor kind."""
-        if self._name_map is None:
-            self._name_map = {}
-            for key, value in self.__class__.__dict__.items():
-                if isinstance(value, self.__class__):
-                    self._name_map[value] = key
-        return self._name_map[self]
-
-    @classmethod
-    def from_id(cls, id):
-        if id >= len(cls._kinds) or cls._kinds[id] is None:
-            raise ValueError('Unknown template argument kind %d' % id)
-        return cls._kinds[id]
-
-    def __repr__(self):
-        return '{}.{}'.format(self.__class__, self.name)
-
 
 class CursorKind(BaseEnumeration):
     """

cb8be5a4ccaca213c9ac1a09766eda1844edbc83
Joan Andrés <andres6936@live.com>
2021-05-29 18:06:18 -0500

    Moved class

---
+++ cldoc/clang/token.py
@@ -0,0 +1,50 @@
+from clang.cursor import Cursor
+from clang.utility.token_kind import TokenKind
+
+
+class Token(Structure):
+    """Represents a single token from the preprocessor.
+
+    Tokens are effectively segments of source code. Source code is first parsed
+    into tokens before being converted into the AST and Cursors.
+
+    Tokens are obtained from parsed TranslationUnit instances. You currently
+    can't create tokens manually.
+    """
+    _fields_ = [
+        ('int_data', c_uint * 4),
+        ('ptr_data', c_void_p)
+    ]
+
+    @property
+    def spelling(self):
+        """The spelling of this token.
+
+        This is the textual representation of the token in source.
+        """
+        return conf.lib.clang_getTokenSpelling(self._tu, self)
+
+    @property
+    def kind(self):
+        """Obtain the TokenKind of the current token."""
+        return TokenKind.from_value(conf.lib.clang_getTokenKind(self))
+
+    @property
+    def location(self):
+        """The SourceLocation this Token occurs at."""
+        return conf.lib.clang_getTokenLocation(self._tu, self)
+
+    @property
+    def extent(self):
+        """The SourceRange this Token occupies."""
+        return conf.lib.clang_getTokenExtent(self._tu, self)
+
+    @property
+    def cursor(self):
+        """The Cursor this Token corresponds to."""
+        cursor = Cursor()
+        cursor._tu = self._tu
+
+        conf.lib.clang_annotateTokens(self._tu, byref(self), 1, byref(cursor))
+
+        return cursor

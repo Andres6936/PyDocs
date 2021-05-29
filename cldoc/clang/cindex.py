4597334dae8bb13b264245b8558145e486e3d130
Joan Andrés <andres6936@live.com>
2021-05-29 17:28:14 -0500

    Moved class

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -1833,32 +1833,6 @@ TypeKind.OCLEVENT = TypeKind(158)
 TypeKind.OCLQUEUE = TypeKind(159)
 TypeKind.OCLRESERVEID = TypeKind(160)
 
-
-
-
-
-
-
-
-class TLSKind(BaseEnumeration):
-    """Describes the kind of thread-local storage (TLS) of a cursor."""
-
-    # The unique kind objects, indexed by id.
-    _kinds = []
-    _name_map = None
-
-    def from_param(self):
-        return self.value
-
-    def __repr__(self):
-        return 'TLSKind.%s' % (self.name,)
-
-
-TLSKind.NONE = TLSKind(0)
-TLSKind.DYNAMIC = TLSKind(1)
-TLSKind.STATIC = TLSKind(2)
-
-
 class Type(Structure):
     """
     The type of an element in the abstract syntax tree.

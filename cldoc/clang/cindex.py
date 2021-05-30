5c01891dc913ea9f69890fd52aa17f6e00404fa5
Joan Andrés <andres6936@live.com>
2021-05-29 20:17:39 -0500

    Import elements

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -91,6 +91,7 @@ from clang.kinds.linkage_kind import LinkageKind
 from clang.kinds.ref_qualifier_kind import RefQualifierKind
 from clang.kinds.template_argument_kind import TemplateArgumentKind
 from clang.kinds.tls_kind import TLSKind
+from clang.objects.completion_string import CompletionString
 from clang.objects.file_inclusion import FileInclusion
 from clang.prototypes.functions import c_object_p, c_interop_string, b, callbacks
 from clang.spelling_cache import SpellingCache
@@ -926,58 +927,6 @@ completionChunkKindMap = {
     18: CompletionChunk.Kind("Equal"),
     19: CompletionChunk.Kind("HorizontalSpace"),
     20: CompletionChunk.Kind("VerticalSpace")}
-
-
-class CompletionString(ClangObject):
-    class Availability:
-        def __init__(self, name):
-            self.name = name
-
-        def __str__(self):
-            return self.name
-
-        def __repr__(self):
-            return "<Availability: %s>" % self
-
-    def __len__(self):
-        return self.num_chunks
-
-    @CachedProperty
-    def num_chunks(self):
-        return conf.lib.clang_getNumCompletionChunks(self.obj)
-
-    def __getitem__(self, key):
-        if self.num_chunks <= key:
-            raise IndexError
-        return CompletionChunk(self.obj, key)
-
-    @property
-    def priority(self):
-        return conf.lib.clang_getCompletionPriority(self.obj)
-
-    @property
-    def availability(self):
-        res = conf.lib.clang_getCompletionAvailability(self.obj)
-        return availabilityKinds[res]
-
-    @property
-    def briefComment(self):
-        if conf.function_exists("clang_getCompletionBriefComment"):
-            return conf.lib.clang_getCompletionBriefComment(self.obj)
-        return _CXString()
-
-    def __repr__(self):
-        return " | ".join([str(a) for a in self]) \
-               + " || Priority: " + str(self.priority) \
-               + " || Availability: " + str(self.availability) \
-               + " || Brief comment: " + str(self.briefComment)
-
-
-availabilityKinds = {
-    0: CompletionChunk.Kind("Available"),
-    1: CompletionChunk.Kind("Deprecated"),
-    2: CompletionChunk.Kind("NotAvailable"),
-    3: CompletionChunk.Kind("NotAccessible")}
 
 
 class CodeCompletionResult(Structure):

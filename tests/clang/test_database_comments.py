5c8803212a3abbffb263710ad5004b56c097e7df
Joan Andrés <andres6936@live.com>
2021-05-30 10:23:23 -0500

    Use of variable

--- tests/clang/test_database_comments.py
+++ tests/clang/test_database_comments.py
@@ -7,11 +7,12 @@ from comments.comments_database import CommentsDatabase
 
 class MyTestCase(unittest.TestCase):
     def test_parser_enum(self):
+        path_file: str = "../input/enum.hh"
         index: Index = Index.create()
-        translation_unit: TranslationUnit = index.parse('../input/enum.hh')
+        translation_unit: TranslationUnit = index.parse(path_file)
         self.assertEqual(len(translation_unit.diagnostics), 0,
                          "The amount of diagnostic in the translation unit not is zero")
-        database_comments = CommentsDatabase('../input/enum.hh', translation_unit)
+        database_comments = CommentsDatabase(path_file, translation_unit)
         self.assertEqual(len(database_comments), 3, "The amount of comments in the database not is 3 (comments)")
         self.assertEqual(repr(database_comments), "Comments: 3", "The representation of object not is equal")
 

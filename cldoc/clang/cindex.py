fcc0406f5cc86a42cc65dd36ce7570bc504e314a
Joan Andrés <andres6936@live.com>
2021-05-29 21:32:37 -0500

    Import elements

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -292,38 +292,6 @@ class File(ClangObject):
         return res
 
 
-class CompileCommand(object):
-    """Represents the compile command used to build a file"""
-
-    def __init__(self, cmd, ccmds):
-        self.cmd = cmd
-        # Keep a reference to the originating CompileCommands
-        # to prevent garbage collection
-        self.ccmds = ccmds
-
-    @property
-    def directory(self):
-        """Get the working directory for this CompileCommand"""
-        return conf.lib.clang_CompileCommand_getDirectory(self.cmd)
-
-    @property
-    def filename(self):
-        """Get the working filename for this CompileCommand"""
-        return conf.lib.clang_CompileCommand_getFilename(self.cmd)
-
-    @property
-    def arguments(self):
-        """
-        Get an iterable object providing each argument in the
-        command line for the compiler invocation as a _CXString.
-
-        Invariant : the first argument is the compiler executable
-        """
-        length = conf.lib.clang_CompileCommand_getNumArgs(self.cmd)
-        for i in range(length):
-            yield conf.lib.clang_CompileCommand_getArg(self.cmd, i)
-
-
 # Now comes the plumbing to hook up the C library.
 
 # Register callback types in common container.
@@ -377,7 +345,6 @@ register_enumerations()
 __all__ = [
     'AvailabilityKind',
     'CodeCompletionResults',
-    'CompileCommand',
     'CursorKind',
     'Cursor',
     'Diagnostic',

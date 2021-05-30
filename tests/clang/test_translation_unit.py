71a8e783285a596f270671d18ba872ee6d42261d
Joan Andrés <andres6936@live.com>
2021-05-30 09:09:07 -0500

    Use of assertEqual

--- tests/clang/test_translation_unit.py
+++ tests/clang/test_translation_unit.py
@@ -11,8 +11,8 @@ class MyTestCase(unittest.TestCase):
         translation_unit: TranslationUnit = index.parse('../input/enum.hh')
         self.assertTrue(len(translation_unit.diagnostics) == 0)
         database_comments = CommentsDatabase('../input/enum.hh', translation_unit)
-        self.assertTrue(len(database_comments) == 3)
-        self.assertTrue(repr(database_comments) == "Comments: 3")
+        self.assertEqual(len(database_comments), 3, "The amount of comments in the database not is 3 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 3", "The representation of object not is equal")
 
 
 if __name__ == '__main__':

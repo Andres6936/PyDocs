f673ccd4cbcc43789f8e3e8ef4e35bba5a871a2f
Joan Andrés <andres6936@live.com>
2021-05-30 07:45:32 -0500

    Optimized imports

--- cldoc/comment.py
+++ cldoc/comment.py
@@ -10,15 +10,12 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc., 51
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import bisect
-import os
 import re
-import sys
 
 import utf8
 from Struct import Struct
-from clang.objects.translation_unit import TranslationUnit
-from clang.utility.token_kind import TokenKind
 
 
 class Sorted(list):

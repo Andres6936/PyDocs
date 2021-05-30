488492c15e48e0e979ec6edf1e6b5681ec25c3eb
Joan Andrés <andres6936@live.com>
2021-05-30 08:36:14 -0500

    Added test to translation unit

---
+++ tests/clang/test_translation_unit.py
@@ -0,0 +1,18 @@
+import unittest
+
+from clang.objects.index import Index
+from clang.objects.translation_unit import TranslationUnit
+from comments.comments_database import CommentsDatabase
+
+
+class MyTestCase(unittest.TestCase):
+    def test_parser_enum(self):
+        index: Index = Index.create()
+        translation_unit: TranslationUnit = index.parse('../input/enum.hh')
+        self.assertTrue(len(translation_unit.diagnostics) == 0)
+        database_comments = CommentsDatabase('../input/enum.hh', translation_unit)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

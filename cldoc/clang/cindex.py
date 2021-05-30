0965e6780831a8203ad7c6354d37d7b5cb09aa91
Joan Andrés <andres6936@live.com>
2021-05-29 20:31:44 -0500

    Import elements

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -100,6 +100,7 @@ from clang.objects.file_inclusion import FileInclusion
 from clang.objects.translation_unit import TranslationUnit
 from clang.prototypes.functions import c_object_p, c_interop_string, b, callbacks, functionList
 from clang.spelling_cache import SpellingCache
+from clang.token import Token
 from clang.token_kinds import TokenKinds
 from clang.type import Type
 from clang.utility.cx_string import _CXString
@@ -602,54 +603,6 @@ class CompilationDatabase(ClangObject):
         the database.
         """
         return conf.lib.clang_CompilationDatabase_getAllCompileCommands(self)
-
-
-class Token(Structure):
-    """Represents a single token from the preprocessor.
-
-    Tokens are effectively segments of source code. Source code is first parsed
-    into tokens before being converted into the AST and Cursors.
-
-    Tokens are obtained from parsed TranslationUnit instances. You currently
-    can't create tokens manually.
-    """
-    _fields_ = [
-        ('int_data', c_uint * 4),
-        ('ptr_data', c_void_p)
-    ]
-
-    @property
-    def spelling(self):
-        """The spelling of this token.
-
-        This is the textual representation of the token in source.
-        """
-        return conf.lib.clang_getTokenSpelling(self._tu, self)
-
-    @property
-    def kind(self):
-        """Obtain the TokenKind of the current token."""
-        return TokenKind.from_value(conf.lib.clang_getTokenKind(self))
-
-    @property
-    def location(self):
-        """The SourceLocation this Token occurs at."""
-        return conf.lib.clang_getTokenLocation(self._tu, self)
-
-    @property
-    def extent(self):
-        """The SourceRange this Token occupies."""
-        return conf.lib.clang_getTokenExtent(self._tu, self)
-
-    @property
-    def cursor(self):
-        """The Cursor this Token corresponds to."""
-        cursor = Cursor()
-        cursor._tu = self._tu
-
-        conf.lib.clang_annotateTokens(self._tu, byref(self), 1, byref(cursor))
-
-        return cursor
 
 
 # Now comes the plumbing to hook up the C library.

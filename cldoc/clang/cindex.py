d2d63a86c57bb1975caf7347fcfb90a09b2d9424
Joan Andrés <andres6936@live.com>
2021-05-29 20:32:11 -0500

    Import elements

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -249,65 +249,6 @@ class SourceRange(Structure):
     def __repr__(self):
         return "<SourceRange start %r, end %r>" % (self.start, self.end)
 
-
-
-class TokenGroup(object):
-    """Helper class to facilitate token management.
-
-    Tokens are allocated from libclang in chunks. They must be disposed of as a
-    collective group.
-
-    One purpose of this class is for instances to represent groups of allocated
-    tokens. Each token in a group contains a reference back to an instance of
-    this class. When all tokens from a group are garbage collected, it allows
-    this class to be garbage collected. When this class is garbage collected,
-    it calls the libclang destructor which invalidates all tokens in the group.
-
-    You should not instantiate this class outside of this module.
-    """
-
-    def __init__(self, tu, memory, count):
-        self._tu = tu
-        self._memory = memory
-        self._count = count
-
-    def __del__(self):
-        conf.lib.clang_disposeTokens(self._tu, self._memory, self._count)
-
-    @staticmethod
-    def get_tokens(tu, extent):
-        """Helper method to return all tokens in an extent.
-
-        This functionality is needed multiple places in this module. We define
-        it here because it seems like a logical place.
-        """
-        tokens_memory = POINTER(Token)()
-        tokens_count = c_uint()
-
-        conf.lib.clang_tokenize(tu, extent, byref(tokens_memory),
-                                byref(tokens_count))
-
-        count = int(tokens_count.value)
-
-        # If we get no tokens, no memory was allocated. Be sure not to return
-        # anything and potentially call a destructor on nothing.
-        if count < 1:
-            return
-
-        tokens_array = cast(tokens_memory, POINTER(Token * count)).contents
-
-        token_group = TokenGroup(tu, tokens_memory, tokens_count)
-
-        for i in range(0, count):
-            token = Token()
-            token.int_data = tokens_array[i].int_data
-            token.ptr_data = tokens_array[i].ptr_data
-            token._tu = tu
-            token._group = token_group
-
-            yield token
-
-
 ## CIndex Objects ##
 
 # CIndex objects (derived from ClangObject) are essentially lightweight

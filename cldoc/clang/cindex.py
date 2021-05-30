106c8536c2855b1f2d819878ae8a29e2c96a32d7
Joan Andrés <andres6936@live.com>
2021-05-29 21:32:11 -0500

    Import elements

--- cldoc/clang/cindex.py
+++ cldoc/clang/cindex.py
@@ -324,53 +324,6 @@ class CompileCommand(object):
             yield conf.lib.clang_CompileCommand_getArg(self.cmd, i)
 
 
-
-class CompilationDatabase(ClangObject):
-    """
-    The CompilationDatabase is a wrapper class around
-    clang::tooling::CompilationDatabase
-
-    It enables querying how a specific source file can be built.
-    """
-
-    def __del__(self):
-        conf.lib.clang_CompilationDatabase_dispose(self)
-
-    @staticmethod
-    def from_result(res, fn, args):
-        if not res:
-            raise CompilationDatabaseError(0,
-                                           "CompilationDatabase loading failed")
-        return CompilationDatabase(res)
-
-    @staticmethod
-    def fromDirectory(buildDir):
-        """Builds a CompilationDatabase from the database found in buildDir"""
-        errorCode = c_uint()
-        try:
-            cdb = conf.lib.clang_CompilationDatabase_fromDirectory(buildDir,
-                                                                   byref(errorCode))
-        except CompilationDatabaseError as e:
-            raise CompilationDatabaseError(int(errorCode.value),
-                                           "CompilationDatabase loading failed")
-        return cdb
-
-    def getCompileCommands(self, filename):
-        """
-        Get an iterable object providing all the CompileCommands available to
-        build filename. Returns None if filename is not found in the database.
-        """
-        return conf.lib.clang_CompilationDatabase_getCompileCommands(self,
-                                                                     filename)
-
-    def getAllCompileCommands(self):
-        """
-        Get an iterable object providing all the CompileCommands available from
-        the database.
-        """
-        return conf.lib.clang_CompilationDatabase_getAllCompileCommands(self)
-
-
 # Now comes the plumbing to hook up the C library.
 
 # Register callback types in common container.
@@ -424,7 +377,6 @@ register_enumerations()
 __all__ = [
     'AvailabilityKind',
     'CodeCompletionResults',
-    'CompilationDatabase',
     'CompileCommand',
     'CursorKind',
     'Cursor',

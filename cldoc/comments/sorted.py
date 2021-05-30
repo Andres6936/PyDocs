85297f33d5e4e5cae75b269c39d6d86b7b77bc6d
Joan Andrés <andres6936@live.com>
2021-05-30 09:16:22 -0500

    Added comments

--- cldoc/comments/sorted.py
+++ cldoc/comments/sorted.py
@@ -1,3 +1,10 @@
+# This module provides support for maintaining a list in sorted order without
+# having to sort the list after each insertion. For long lists of items with
+# expensive comparison operations, this can be an improvement over the more
+# common approach. The module is called bisect because it uses a basic
+# bisection algorithm to do its work. The source code may be most useful as
+# a working example of the algorithm (the boundary conditions are already
+# right!).
 import bisect
 
 

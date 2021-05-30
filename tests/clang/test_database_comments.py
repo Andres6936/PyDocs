fd432929909ce5e8fdbb6c51809f288f0dd303a2
Joan Andrés <andres6936@live.com>
2021-05-30 10:45:14 -0500

    Added tests

--- tests/clang/test_database_comments.py
+++ tests/clang/test_database_comments.py
@@ -26,43 +26,69 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(repr(database_comments), "Comments: 3", "The representation of object not is equal")
 
     def test_parser_abstract(self):
-        pass
+        database_comments = self.get_comments_database("../input/abstract.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
     def test_parser_base(self):
-        pass
+        database_comments = self.get_comments_database("../input/base.hh")
+        self.assertEqual(len(database_comments), 3, "The amount of comments in the database not is 3 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 3", "The representation of object not is equal")
 
     def test_parser_constructor(self):
-        pass
+        database_comments = self.get_comments_database("../input/constructor.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
     def test_parser_destructor(self):
-        pass
+        database_comments = self.get_comments_database("../input/destructor.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
     def test_parser_interface(self):
-        pass
+        database_comments = self.get_comments_database("../input/interface.hh")
+        self.assertEqual(len(database_comments), 4, "The amount of comments in the database not is 4 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 4", "The representation of object not is equal")
 
     def test_parser_method(self):
-        pass
+        database_comments = self.get_comments_database("../input/method.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
     def test_parser_namespace(self):
-        pass
+        database_comments = self.get_comments_database("../input/namespace.hh")
+        self.assertEqual(len(database_comments), 6, "The amount of comments in the database not is 6 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 6", "The representation of object not is equal")
 
     def test_parser_struct(self):
-        pass
+        database_comments = self.get_comments_database("../input/struct.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
     def test_parser_template(self):
-        pass
+        database_comments = self.get_comments_database("../input/template.hh")
+        self.assertEqual(len(database_comments), 2, "The amount of comments in the database not is 2 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 2", "The representation of object not is equal")
 
     def test_parser_union(self):
-        pass
+        database_comments = self.get_comments_database("../input/union.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
     def test_parser_union_struct(self):
-        pass
+        database_comments = self.get_comments_database("../input/unionanonstruct.hh")
+        self.assertEqual(len(database_comments), 5, "The amount of comments in the database not is 5 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 5", "The representation of object not is equal")
 
     def test_parser_utf8(self):
-        pass
+        database_comments = self.get_comments_database("../input/utf8.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
     def test_parser_virtual(self):
-        pass
+        database_comments = self.get_comments_database("../input/virtual.hh")
+        self.assertEqual(len(database_comments), 1, "The amount of comments in the database not is 1 (comments)")
+        self.assertEqual(repr(database_comments), "Comments: 1", "The representation of object not is equal")
 
 
 if __name__ == '__main__':
